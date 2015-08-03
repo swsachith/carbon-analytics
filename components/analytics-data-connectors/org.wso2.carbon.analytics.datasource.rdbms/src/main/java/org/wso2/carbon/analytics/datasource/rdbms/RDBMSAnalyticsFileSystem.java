308ee7cd573137d752f9497053a94a802a813d8b
Sachith Withana <swsachith@gmail.com>
2015-08-03 14:59:12 +0530

    adding filesystem backing up and restoring feature to the backup tool - BAM-2101

--- components/analytics-data-connectors/org.wso2.carbon.analytics.datasource.rdbms/src/main/java/org/wso2/carbon/analytics/datasource/rdbms/RDBMSAnalyticsFileSystem.java
+++ components/analytics-data-connectors/org.wso2.carbon.analytics.datasource.rdbms/src/main/java/org/wso2/carbon/analytics/datasource/rdbms/RDBMSAnalyticsFileSystem.java
@@ -243,7 +243,8 @@ public class RDBMSAnalyticsFileSystem implements AnalyticsFileSystem {
 
     @Override
     public List<String> list(String path) throws IOException {
-        path = GenericUtils.normalizePath(path);
+        //todo resolve this issue
+        //path = GenericUtils.normalizePath(path);
         Connection conn = null;
         try {
             conn = this.getConnection();
@@ -263,8 +264,9 @@ public class RDBMSAnalyticsFileSystem implements AnalyticsFileSystem {
             stmt.setString(1, path);
             rs = stmt.executeQuery();
             List<String> result = new ArrayList<String>();
+            int trimLength = path.equals("/") ? 1 : path.length() + 1;
             while (rs.next()) {
-                result.add(rs.getString(1).substring(path.length() + 1));
+                result.add(rs.getString(1).substring(trimLength));
             }
             return result;
         } catch (SQLException e) {
